b421ef6b2b83fb3ff9e98c66e8b25629fd6f0139
Jayabharathi S <jayabharathi.s@thoughtworks.com>
2024-03-12 20:56:57 +0530

    add generic object jp working

--- java/src/main/java/com/google/developers/wallet/rest/temp.java
+++ java/src/main/java/com/google/developers/wallet/rest/temp.java
@@ -21,7 +21,7 @@ import java.util.*;
 class HelloWorld {
     public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
 //        DemoGeneric demoGeneric = DemoGeneric( );
-        final String jwt = createJWTNewObjects("3388000000022327001", "plastic", "plastic");
+        final String jwt = createJWTNewOneObjects("3388000000022327001", "red9", "red9");
         System.out.println(jwt);
 
 
@@ -211,6 +211,172 @@ class HelloWorld {
 
         return String.format("https://pay.google.com/gp/v/save/%s", token);
     }
+
+    public static String createJWTNewOneObjects(String issuerId, String classSuffix, String objectSuffix) throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
+        // See link below for more information on required properties
+        // https://developers.google.com/wallet/generic/rest/v1/genericclass
+        GenericClass newClass = new GenericClass().setId(String.format("%s.%s", issuerId, classSuffix));
+
+        // See link below for more information on required properties
+        // https://developers.google.com/wallet/generic/rest/v1/genericobject
+        GenericObject newObject =
+                new GenericObject()
+                        .setId(String.format("%s.%s", issuerId, objectSuffix))
+                        .setClassId(String.format("%s.%s", issuerId, classSuffix))
+                        .setState("ACTIVE")
+                        .setHeroImage(
+                                new Image()
+                                        .setSourceUri(
+                                                new ImageUri()
+                                                        .setUri(
+                                                                "https://raw.githubusercontent.com/droidbg/assets/main/sea_horse.png"))
+                                        .setContentDescription(
+                                                new LocalizedString()
+                                                        .setDefaultValue(
+                                                                new TranslatedString()
+                                                                        .setLanguage("en-US")
+                                                                        .setValue("Sea horse is happy"))))
+                        .setTextModulesData(
+                                List.of(
+                                        new TextModuleData()
+                                                .setHeader("おめでとう！")
+                                                .setBody("美しいタツノオトシゴを救出しました")
+                                                .setId("TEXT_MODULE_ID")))
+                        .setLinksModuleData(
+                                new LinksModuleData()
+                                        .setUris(
+                                                Arrays.asList(
+                                                        new Uri()
+                                                                .setUri("https://jayabha-jayas.github.io/ocean_cleanup/")
+                                                                .setDescription("オーシャンクリーンアップをプレイする")
+                                                                .setId("LINK_MODULE_URI_ID")
+//                                                        new Uri()
+//                                                                .setUri("tel:6505555555")
+//                                                                .setDescription("Link module tel description")
+//                                                                .setId("LINK_MODULE_TEL_ID")
+                                                )
+                                        ))
+
+                        .setImageModulesData(
+                                List.of(
+//                                        new ImageModuleData()
+//                                                .setMainImage(
+//                                                        new Image()
+//                                                                .setSourceUri(
+//                                                                        new ImageUri()
+//                                                                                .setUri(
+//                                                                                        "https://raw.githubusercontent.com/droidbg/assets/main/sea_horse.png"))
+//                                                                .setContentDescription(
+//                                                                        new LocalizedString()
+//                                                                                .setDefaultValue(
+//                                                                                        new TranslatedString()
+//                                                                                                .setLanguage("en-US")
+//                                                                                                .setValue("Image module description"))))
+//                                                .setId("IMAGE_MODULE_ID")
+                                ))
+//                        .setBarcode(new Barcode().setType("QR_CODE").setValue("QR code value"))
+                        .setCardTitle(
+                                new LocalizedString()
+                                        .setDefaultValue(
+                                                new TranslatedString().setLanguage("en-US").setValue("海洋浄化")))
+                        .setHeader(
+                                new LocalizedString()
+                                        .setDefaultValue(
+                                                new TranslatedString().setLanguage("en-US").setValue("海上戦闘機")))
+                        .setHexBackgroundColor("#D9F6F0")
+                        .setLogo(
+                                new Image()
+                                        .setSourceUri(
+                                                new ImageUri()
+                                                        .setUri(
+                                                                "https://raw.githubusercontent.com/droidbg/assets/main/under_ocean.png"))
+                                        .setContentDescription(
+                                                new LocalizedString()
+                                                        .setDefaultValue(
+                                                                new TranslatedString()
+                                                                        .setLanguage("en-US")
+                                                                        .setValue("Generic card logo"))));
+
+        // Create the JWT as a HashMap object
+        HashMap<String, Object> claims = new HashMap<String, Object>();
+        claims.put("iss", "abcd-654@abcd-416109.iam.gserviceaccount.com");
+        claims.put("aud", "google");
+        claims.put("origins", List.of("www.example.com"));
+        claims.put("typ", "savetowallet");
+
+        // Create the Google Wallet payload and add to the JWT
+        HashMap<String, Object> payload = new HashMap<String, Object>();
+        payload.put("genericClasses", List.of(newClass));
+        payload.put("genericObjects", List.of(newObject));
+        claims.put("payload", payload);
+        final String PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\n" +
+                "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC24ha25s/Jc33T\n" +
+                "zJUkQzkXFiS0C5vh1kDwdWSjkNresqXngaH6uqckfKa1MF0CxelJwp7KVp7hASoJ\n" +
+                "N9T0ZGLsRq+AjA4Otk3B7QEzj10Q1gramo0Egf6y8FCS819Kz8mzuUQk/um3GbNn\n" +
+                "PizFOIKYBnZ8RJo7DZ6Dy9p4g88vFj54JmqJzitQf7m5VfvTl6N7fA7V3Ffq7Ki+\n" +
+                "Uol1m10/QwjEYFtD/QFd20FBMW5/L/JM7yF63BAdt/djaEWQUNZAV/1HXZQv9oMP\n" +
+                "LMSB8p4UEAicSBY9bjtecY/2AKc8Ubtghb0tnrtLn+VmzYHtj5h9EcA0drivFLLO\n" +
+                "58QBhVRPAgMBAAECggEABg/1gTNNHPP5dtI9G2goZOu47PL5kQFlqQYeZbkfE/w/\n" +
+                "H8iePRrDzV9/6Ix/I3p2CTOTHjf3ILZwTOMgUB2lpl2z4rW5Sl7kj8XdS7sejA4g\n" +
+                "nh1L9858bDTjBL875CsKwtdo5b6n5aGYiZbeX1QU9gewSBGdTiahwfTTQCkyId/d\n" +
+                "8mF6sA5J1IOc4+AW7l+rBOwttFA5Fdkyn19DdEZ1TNcXqHAXr4QOztRpxMCCB58s\n" +
+                "B5ctPUPU9GaMGBaHjnYzJDnP//Xj7oytgtouo/DSMtY2sl4dLez8Yb/xa1IIZyOh\n" +
+                "8Dv4VtFGRRVRr1tKuR+1hPNBPC0OGXaxzek70quMGQKBgQDaUAMWJfN/J3j3cFFX\n" +
+                "Znc/w2oe58Zdv/GfltTBZotAUEfMFkgQTtRvyTqtzW4JdDGH2U2MVUCJnbvGu+Z7\n" +
+                "MeJWXWmPI3OOg9UKqqFLcEnUYAz8NwSNzcjxZmVdhXs9n/ANZ0LtY+33Tf3g2snN\n" +
+                "IeC7ZKzqjS1hHCw3l8gYcTQQrQKBgQDWdFRJkH6DQx5diekDKTBYfnqREzKiuzJ5\n" +
+                "ayTdiUWNPNWMcPlZf3fX94fMd25JPcoxD5nxcuCnqehp2VOHnk4iBW6YmsVjGQsx\n" +
+                "EcmmNc4OOSr42C21KbNsuyJQpVSGyHj2kelqLF/nBnDN6HsnTyPJImO+jbJz8XZd\n" +
+                "z1/SQetMawKBgETfBwo+sMGFb1kIOvEKp0YbXOghEGhU/GHvUHEu63E/olbVFKjo\n" +
+                "ZYZagkOAUl1zJYkI2bkS0AhaFXoNnxlwVEoQsBNzCKqGFniF+dp1syTnpFEQ/kPK\n" +
+                "DsFxHztQt99qDG3+DJQdeV4SYaxE+XLYZDBaA17/aLiBDcYAASUM2XMFAoGAf9xH\n" +
+                "zLyL0XMZDagF9059dqWiKhEKJZ/6zg2lozgO8dCNefh5knS/XVJF8xBZ/u0KOgoV\n" +
+                "PLHDY/OXkFR0yu0FpJ8ce3v7p8PsukrisWhu/Gf1SPyK8/Jy+abxJKgOJ3YrOMdy\n" +
+                "JU1juHhMlVfA5YT3zxnj4l7VQO98JR5ZASEwSxUCgYBvr0PBcsImhTgSJtOcoXr3\n" +
+                "jEnQ9kx9DhWJp9tNGGBjXozcsiHtPu211fgqyQGnnCf0WssvrXg+YntjKDzUkZkK\n" +
+                "hwjwwo0TuJZ3qidzekgdeTzQT4/nI1gds0v6WSUiSHodJkBoOeuwZnGtwz4/IEPo\n" +
+                "fqtelugALXdqY4Iw79s2Qg==\n" +
+                "-----END PRIVATE KEY-----";
+
+// Read in the key into a String
+        StringBuilder pkcs8Lines = new StringBuilder();
+        BufferedReader rdr = new BufferedReader(new StringReader(PRIVATE_KEY));
+        String line;
+        while ((line = rdr.readLine()) != null) {
+            pkcs8Lines.append(line);
+        }
+
+        // Remove the "BEGIN" and "END" lines, as well as any whitespace
+
+        String pkcs8Pem = pkcs8Lines.toString();
+        pkcs8Pem = pkcs8Pem.replace("-----BEGIN PRIVATE KEY-----", "");
+        pkcs8Pem = pkcs8Pem.replace("-----END PRIVATE KEY-----", "");
+        pkcs8Pem = pkcs8Pem.replaceAll("\\s+", "");
+
+        // Base64 decode the result
+
+//        byte [] pkcs8EncodedBytes = Base64.decode(pkcs8Pem, Base64.DEFAULT);
+        byte[] pkcs8EncodedBytes = Base64.getDecoder().decode(pkcs8Pem);
+        // extract the private key
+
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(pkcs8EncodedBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        PrivateKey privKey = kf.generatePrivate(keySpec);
+        System.out.println(privKey);
+//RSAPrivateKey rsaPrivateKey1 = RSAPrivateKey()
+
+        // The service account credentials are used to sign the JWT
+        Algorithm algorithm =
+                Algorithm.RSA256(
+                        null, (RSAPrivateKey) privKey);
+        String token = JWT.create().withPayload(claims).sign(algorithm);
+
+        System.out.println("Add to Google Wallet link");
+        System.out.printf("https://pay.google.com/gp/v/save/%s%n", token);
+
+        return String.format("https://pay.google.com/gp/v/save/%s", token);
+    }
+    // [END jwtNew]
 
     @NotNull
     private static Map<String, Object> getPayload() {
